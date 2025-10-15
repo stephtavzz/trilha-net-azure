497c2709900796c1d8fce75ccc344f0cb29d46d3
stephtavzz <stephanie.tavares.pe@gmail.com>
2025-10-15 12:58:11 -0300

    Publicando projeto na nuvem

--- obj/Debug/net9.0/TrilhaNetAzureDesafio.AssemblyInfo.cs
+++ obj/Debug/net9.0/TrilhaNetAzureDesafio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrilhaNetAzureDesafio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22848cf6b3f98fec96da3b1b1bf8981205c80eef")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrilhaNetAzureDesafio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrilhaNetAzureDesafio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
